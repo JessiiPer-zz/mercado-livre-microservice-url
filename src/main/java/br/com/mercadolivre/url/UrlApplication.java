a825783fbbfe39b9433f41236aa5c79c488909cc
JessiiPer <jessica_roch@hotmail.com>
2020-01-27 16:16:46 -0300

    Implementação da classe de configuração para autenticação e autorização do serviço

--- src/main/java/br/com/mercadolivre/url/UrlApplication.java
+++ src/main/java/br/com/mercadolivre/url/UrlApplication.java
@@ -2,8 +2,10 @@ package br.com.mercadolivre.url;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
+@EnableResourceServer
 public class UrlApplication {
 
 	public static void main(String[] args) {
